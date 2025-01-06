bf65877c881cd2c050ad42e21b4af99d7c40c3d4
Musaveer <mvmanekia2002@gmail.com>
2025-01-06 12:25:07 +0530

    for web user login and update, added readme file

--- index.js
+++ index.js
@@ -3,15 +3,23 @@ console.log('External JS file linked');
 // Check if the CleverTap SDK is loaded
 clevertap.onUserLogin.push({
     "Site": {
-        "Name": "anonymous",            // String
-        "Identity": 61026030,           // String or number
-        "Email": "demo@gmail.com",      // Email address of the user
+        "Name": "clever5",            // String
+        "Identity": 61225905,           // String or number
+        "Email": "clever5@gmail.com",      // Email address of the user
         "Phone": "+14155551234",        // Phone (with the country code)
-        "Gender": "M",                  // Can be either M or F
         "DOB": new Date(),              // Date of Birth. Date object
         "MSG-email": false,             // Disable email notifications
         "MSG-push": true,               // Enable push notifications
         "MSG-sms": true,                // Enable sms notifications
         "MSG-whatsapp": true            // Enable WhatsApp notifications
+    }
+});
+
+// this method also work for adding but is usually used for update
+// more info about this in README file
+
+clevertap.profile.push({
+    "Site":{
+        "Gender": "M",
     }
 });
