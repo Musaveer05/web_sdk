cd6f69883c1b2273cdffb85ae70c9176e5d9b1d1
Musaveer <mvmanekia2002@gmail.com>
2025-01-06 16:04:34 +0530

    my clevertap id integration

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ console.log('External JS file linked');
 // Check if the CleverTap SDK is loaded
 clevertap.onUserLogin.push({
     "Site": {
-        "Name": "clever5",            // String
-        "Identity": 61225905,           // String or number
-        "Email": "clever5@gmail.com",      // Email address of the user
+        "Name": "mv1",            // String
+        "Identity": 61225901,           // String or number
+        "Email": "mv1@gmail.com",      // Email address of the user
         "Phone": "+14155551234",        // Phone (with the country code)
         "DOB": new Date(),              // Date of Birth. Date object
         "MSG-email": false,             // Disable email notifications
@@ -15,7 +15,7 @@ clevertap.onUserLogin.push({
     }
 });
 
-// this method also work for adding but is usually used for update
+// this method works for updating.
 // more info about this in README file
 
 clevertap.profile.push({
