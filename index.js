65c2cd436b289b2c03fdf381e89006b171b4b233
Musaveer <mvmanekia2002@gmail.com>
2025-02-19 17:57:06 +0530

    .

--- index.js
+++ index.js
@@ -51,16 +51,16 @@ console.log(`The Identity is ${randomIdentity}`);
 //     }
 // });
 
-// document.getElementById('pushNotifBtn').addEventListener('click', function () {
-//     console.log('tapped');
-//     clevertap.notifications.push({
-//         "titleText": 'Would you like to receive Push Notifications?',
-//         "bodyText": 'We promise to only send you relevant content and give you updates on your transactions',
-//         "okButtonText": 'Sign me up!',
-//         "rejectButtonText": 'No thanks',
-//         "okButtonColor": '#f28046'
-//     });
-// })
+document.getElementById('pushNotifBtn').addEventListener('click', function () {
+    console.log('tapped');
+    clevertap.notifications.push({
+        "titleText": 'Would you like to receive Push Notifications?',
+        "bodyText": 'We promise to only send you relevant content and give you updates on your transactions',
+        "okButtonText": 'Sign me up!',
+        "rejectButtonText": 'No thanks',
+        "okButtonColor": '#f28046'
+    });
+})
 
 function handlePopup() {
     var overlay = window.parent.document.getElementById('wizpardiv0');
