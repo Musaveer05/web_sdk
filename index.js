2a4c49ed5bbf4d0a27969b59d68f99e6885f252b
Musaveer05 <93473939+Musaveer05@users.noreply.github.com>
2025-04-24 18:56:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -52,6 +52,14 @@ clevertap.onUserLogin.push({
     }
 });
 
+clevertap.event.push("File Uploaded", {
+  "File Name": "resume.pdf",
+  "File Size (KB)": 256,
+  "File Type": "application/pdf",
+  "Upload Time": new Date().toISOString(),
+  "User Email": randomEmail
+});
+
 console.log(`The emailId is ${randomEmail}`);
 console.log(`The Identity is ${randomIdentity}`);
 
