6f9c2b89b265da4f7d2fdde7717b81ced8d496b3
Musaveer05 <93473939+Musaveer05@users.noreply.github.com>
2025-09-02 19:53:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,31 +27,31 @@ console.log('External JS file linked');
 
 
 // // call this when a user login form is submitted.
-// clevertap.onUserLogin.push({
-//     "Site": {
-//         "Identity": "989197402610",    // Random numeric string of length 8
-//         "Phone":"+91123432404359234"
-//         "MSG-email": true,            // Disable email notifications
-//         "MSG-push": true,              // Enable push notifications
-//         "MSG-sms": true,               // Enable sms notifications
-//         "MSG-whatsapp": false           // Enable WhatsApp notifications
-//     }
-// });
+clevertap.onUserLogin.push({
+    "Site": {
+        "Identity": "989197402610",    // Random numeric string of length 8
+        "Phone":"+918691951130"
+        "MSG-email": true,            // Disable email notifications
+        "MSG-push": true,              // Enable push notifications
+        "MSG-sms": true,               // Enable sms notifications
+        "MSG-whatsapp": false           // Enable WhatsApp notifications
+    }
+});
 
 // console.log(`The emailId is ${randomEmail}`);
 // console.log(`The Identity is ${randomIdentity}`);
 
 // for web push notification.
-// document.getElementById('pushNotifBtn').addEventListener('click', function () {
-//     console.log('tapped');
-//     clevertap.notifications.push({
-//         "titleText": 'Would you like to receive Push Notifications?',
-//         "bodyText": 'We promise to only send you relevant content and give you updates on your transactions',
-//         "okButtonText": 'Sign me up!',
-//         "rejectButtonText": 'No thanks',
-//         "okButtonColor": '#f28046'
-//     });
-// })
+document.getElementById('pushNotifBtn').addEventListener('click', function () {
+    console.log('tapped');
+    clevertap.notifications.push({
+        "titleText": 'Would you like to receive Push Notifications?',
+        "bodyText": 'We promise to only send you relevant content and give you updates on your transactions',
+        "okButtonText": 'Sign me up!',
+        "rejectButtonText": 'No thanks',
+        "okButtonColor": '#f28046'
+    });
+})
 
 document.getElementById('productviewed').addEventListener('click', function(){
     console.log('prod viewed');
