3aa9bb3fdf99c4f6dfb108f7772a6086181524cb
Musaveer05 <93473939+Musaveer05@users.noreply.github.com>
2026-03-16 10:10:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -91,11 +91,13 @@ var num2 = 12345;     // integer
 document.getElementById('productviewed').addEventListener('click', function () {
     console.log('Raised test_event_01 having Date prop');
 
-    // Event with string order_id
-    clevertap.event.push("test_event_01", {
-    "date": new Date(2026, 2, 16, 10, 05, 0),
-        "test_prop": "Eng"
+    const eventDate = Math.floor(new Date(2026, 2, 16, 10, 15, 0).getTime() / 1000); // Convert to epoch seconds
+cleverTap.event.push("test_event_01", {
+    "date": `$D_${eventDate}`,
+    "test_prop": "Eng"
 });
+
+
 
     // Event with integer order_id
     // clevertap.event.push("Child eSIM Activated", {
